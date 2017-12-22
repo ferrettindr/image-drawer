b7a98d42dea50a5ae6b95e3e7b64fabb2fefc77b
Andrea Ferretti <ferretti.ndr@gmail.com>
2017-12-22 15:23:27 +0100

    Refactor SquareBrush as a subclass of Rectangle
    By making SquareBrush a subcalss of RectangleBrush a lot of duplicate
    code gets removed.

--- drawer/SquareBrush.java
+++ drawer/SquareBrush.java
@@ -1,53 +1,9 @@
 package drawer;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.awt.Graphics2D;
-
-public class SquareBrush extends AbstractBrush {
-
-	private int side;
+public class SquareBrush extends RectangleBrush {
 
 	public SquareBrush(int size) {
-		side = size;
+		super(size, size);
 	}
 
-	protected boolean tryDraw(BufferedImage sourceImg, BufferedImage destImg, int x, int y, Color newColor) {
-		double oldDiff = 0, newDiff = 0;
-		Color oldColor;
-		
-		int width, height;
-		
-		//(x,y) is the center
-		if (x < side/2)
-			width = 0;
-		else
-			width = x - side/2;
-
-		while ((width < sourceImg.getWidth()) && (width < x+side/2)) {
-			if (y < side/2)
-				height = 0;
-			else
-				height = y - side/2;
-			while ((height < sourceImg.getHeight()) && (height < y+side/2)) {
-
-				oldColor = new Color(sourceImg.getRGB(width, height), true);
-
-				oldDiff += colorDistance(oldColor, new Color(destImg.getRGB(width, height), true));
-				newDiff += colorDistance(oldColor, newColor);
-
-				height++;
-			}
-			width++;
-		}
-
-		return newDiff < oldDiff;
-		
-	}
-
-	protected void draw(Graphics2D drawer, int x, int y, Color newColor) {
-		drawer.setColor(newColor);
-		//(x,y) is the center
-		drawer.fillRect(x-(side/2), y-(side/2), side, side);
-	}
 }
