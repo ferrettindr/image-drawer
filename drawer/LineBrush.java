84f636d86f4c912fa0c715d257708b72eb9ed463
Andrea Ferretti <ferretti.ndr@gmail.com>
2018-01-12 00:18:43 +0100

    Fix tan(90°) case when drawing line at 90°
    By changing the pivot from X to Y in the drawing of the line, a lot of
    corner cases caused by having an angle of 90 degree resolve themselves.

--- drawer/LineBrush.java
+++ drawer/LineBrush.java
@@ -19,31 +19,30 @@ public class LineBrush extends AbstractBrush {
 	public boolean tryDraw(BufferedImage sourceImg, BufferedImage destImg, int x, int y, Color newColor) {
 		double oldDiff = 0, newDiff = 0;
 		Color oldColor;
-		
+
 		for (int thick = 0; thick < thickness; thick++) {
 			int newX = x + thick, newY = y + thick;
-			//use the linear equation   y = mx + constant   to find the other points of the line
-			double constant = newY-(slope*newX);
 
 			int xAxis, yAxis;
 
-			if (x < length/2)
-				xAxis = 0;
-			else
-				xAxis = newX - length/2;
-
 			//(x,y) is the center
-			for (int len = 0; len < length && xAxis < (x+length/2); len++) {
-				yAxis = (int)(slope * xAxis + constant);
+			if (y < length/2)
+				yAxis = 0;
+			else
+				yAxis = newY - length/2;
 
-				if ((xAxis >= sourceImg.getWidth()) || (yAxis < 0) || (yAxis >= sourceImg.getHeight()))
+			for (int len = 0; len < length && yAxis < (y+length/2); len++) {
+				//use the linear equation   y = mx + constant   to find the other points of the line
+				xAxis = (int)((yAxis - newY)/slope + newX);
+
+				if ((yAxis >= sourceImg.getHeight()) || (xAxis < 0) || (xAxis >= sourceImg.getWidth()))
 					break;
 
 				oldColor = new Color(sourceImg.getRGB(xAxis, yAxis), true);
 				oldDiff += colorDistance(oldColor, new Color(destImg.getRGB(xAxis, yAxis), true));
 				newDiff += colorDistance(oldColor, newColor);
 				
-				xAxis++;
+				yAxis++;
 			}
 		}
 		return newDiff < oldDiff;
